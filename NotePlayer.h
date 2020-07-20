d6ecd7f547cdb0ab8424cdd628310d8265e8d14f
LeoTheLegion <mmena2017@fau.edu>
2020-07-20 19:07:49 -0400

    Made a small jingle to test system

--- NotePlayer.h
+++ NotePlayer.h
@@ -14,7 +14,23 @@
 typedef enum Notes{
     Testnote1 = 500,
     Testnote2 = 1000,
-    Testnote3 = 1500
+    Testnote3 = 1500,
+    C6 = 1046,
+    D6 = 1174,
+    Ds6 = 1244,
+    E6 = 1318,
+    F6 = 1396,
+    Fs6 = 1479,
+    G6 = 1567,
+    Gs6 = 1661,
+    A6 =  1760,
+    As6 = 1864,
+    B6 = 1975,
+    E7 = 2637,
+    F7 = 2793,
+    G7 = 3135,
+    A7 = 3520,
+    B7 = 3951
 }Notes;
 
 unsigned int timeElapsed_Target_ms = 0;
--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 
 #define LED1 BIT1
 #define LED2 BIT2
+#define TICKS_PER_BEAT 4
 
+static unsigned int ms_per_tick = 0;
 
 void init(){
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
@@ -18,19 +20,119 @@ void init(){
     _enable_interrupt();
 }
 
+void TestC5Major()
+{
+    PlayNote(C6, 500);
+    PlayNote(D6, 500);
+    PlayNote(E6, 500);
+    PlayNote(F6, 500);
+    PlayNote(G6, 500);
+    PlayNote(A6, 500);
+    PlayNote(B6, 500);
+    PlayNote(B6, 500);
+    PlayNote(A6, 500);
+    PlayNote(G6, 500);
+    PlayNote(F6, 500);
+    PlayNote(E6, 500);
+    PlayNote(D6, 500);
+    PlayNote(C6, 500);
+}
+
+void set_bpm(unsigned int bpm) {
+    ms_per_tick = 60000 / (TICKS_PER_BEAT * bpm);
+}
+
 int main(void)
 {
     init();
 
     while(1){
-        PlayNote(Testnote1, 1000);
-        PlayNote(Testnote2, 1000);
-        Pause(1000);
-        PlayNote(Testnote1, 500);
-        PlayNote(Testnote3, 500);
-        PlayNote(Testnote1, 500);
-        PlayNote(Testnote2, 500);
-        PlayNote(Testnote3, 500);
-        PlayNote(Testnote2, 500);
+        //TestC5Major();
+        set_bpm(120);
+
+        PlayNote(E6,1 * ms_per_tick);
+        PlayNote(E6,1 * ms_per_tick);
+        Pause(1 * ms_per_tick);
+        PlayNote(E6,1* ms_per_tick);
+        Pause(1 * ms_per_tick);
+        PlayNote(C6,1* ms_per_tick);
+        PlayNote(E6,1* ms_per_tick);
+        Pause(1* ms_per_tick);
+
+        PlayNote(F7,1* ms_per_tick);
+        Pause(3* ms_per_tick);
+        PlayNote(G6,1* ms_per_tick);
+        Pause(3* ms_per_tick);
+/////////////////////////////////
+
+        PlayNote(C6,1 * ms_per_tick);
+        Pause(2* ms_per_tick);
+        PlayNote(G7,1 * ms_per_tick);
+        Pause(2* ms_per_tick);
+        PlayNote(E7,1 * ms_per_tick);
+        Pause(1* ms_per_tick);
+
+        Pause(1* ms_per_tick);
+        PlayNote(A7,1 * ms_per_tick);
+        Pause(1* ms_per_tick);
+        PlayNote(B7,1 * ms_per_tick);
+        Pause(1* ms_per_tick);
+        PlayNote(As6,1 * ms_per_tick);
+        PlayNote(A6,1 * ms_per_tick);
+        Pause(1* ms_per_tick);
+
+        PlayNote(G7,1 * ms_per_tick);
+        PlayNote(E6,1 * ms_per_tick);
+        Pause(1* ms_per_tick);
+        PlayNote(G6,1 * ms_per_tick);
+        PlayNote(A6,1 * ms_per_tick);
+        Pause(1* ms_per_tick);
+        PlayNote(F6,1 * ms_per_tick);
+        PlayNote(G6,1 * ms_per_tick);
+
+        Pause(1* ms_per_tick);
+        PlayNote(E6,1 * ms_per_tick);
+        Pause(1* ms_per_tick);
+        PlayNote(C6,1 * ms_per_tick);
+        PlayNote(D6,1 * ms_per_tick);
+        PlayNote(B7,1 * ms_per_tick);
+        Pause(1* ms_per_tick);
+
+
+ //////////////////////////////////////////
+        PlayNote(C6,1 * ms_per_tick);
+       Pause(2* ms_per_tick);
+       PlayNote(G7,1 * ms_per_tick);
+       Pause(2* ms_per_tick);
+       PlayNote(E7,1 * ms_per_tick);
+       Pause(1* ms_per_tick);
+
+       Pause(1* ms_per_tick);
+       PlayNote(A7,1 * ms_per_tick);
+       Pause(1* ms_per_tick);
+       PlayNote(B7,1 * ms_per_tick);
+       Pause(1* ms_per_tick);
+       PlayNote(As6,1 * ms_per_tick);
+       PlayNote(A6,1 * ms_per_tick);
+       Pause(1* ms_per_tick);
+
+       PlayNote(G7,1 * ms_per_tick);
+       PlayNote(E6,1 * ms_per_tick);
+       Pause(1* ms_per_tick);
+       PlayNote(G6,1 * ms_per_tick);
+       PlayNote(A6,1 * ms_per_tick);
+       Pause(1* ms_per_tick);
+       PlayNote(F6,1 * ms_per_tick);
+       PlayNote(G6,1 * ms_per_tick);
+
+       Pause(1* ms_per_tick);
+       PlayNote(E6,1 * ms_per_tick);
+       Pause(1* ms_per_tick);
+       PlayNote(C6,1 * ms_per_tick);
+       PlayNote(D6,1 * ms_per_tick);
+       PlayNote(B7,1 * ms_per_tick);
+       Pause(1* ms_per_tick);
+
+//////////////////////////////////////
     }
 }
