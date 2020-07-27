fcec51f9e65c9b83af78d4f4549561fe19533e46
LeoTheLegion <mmena2017@fau.edu>
2020-07-27 19:54:57 -0400

    Finish Rough draft of pokemon. It still needs work

--- main.c
+++ main.c
@@ -206,49 +206,49 @@ void PM_measure10()
 
 void PM_measure11()
 {
-    Play(E4);//full note
-    Play(E4);//full note
-    Play(E4);//full note
-    Play(E4);//full note
-    Play(E4);//full note
-        Play(E4);//full note
+    Play(E4);//half note
+    Play(E4);//half note
+    Play(E4);//half note
+    Play(E4);//half note
+    Play(E4);//half note
+        Play(E4);//half note
     Play(B4);
-    Play(Eb4);//full note
-    Play(Eb4);//full note
-    Play(Eb4);//full note
-    Play(Eb4);//full note
-    Play(Eb4);//full note
-        Play(Eb4);//full note
+    Play(Eb4);//half note
+    Play(Eb4);//half note
+    Play(Eb4);//half note
+    Play(Eb4);//half note
+    Play(Eb4);//half note
+        Play(Eb4);//half note
     Play(Bb4);
 }
 
 void PM_measure12()
 {
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
     Play(A3);
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
     Play(As3);
 }
 
 void PM_measure13()
 {
     Play(As3);
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
     Play(F3);
     Play(E3);
     Play(F3);
@@ -266,9 +266,11 @@ void PM_measure14()
     Play(A4);
     Play(B4);
     Play(A4);
+
     Play(C4);
     Play(B4);
     Rest(1); //sixteenth Rest
+
     Play(A4);
     Play(B4);
     Play(B4);
@@ -289,55 +291,55 @@ void PM_measure16()
 void PM_measure17()
 {
     Play(As3);
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
-    Play(B3); //full note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
+    Play(B3); //half note
 }
 
 void PM_measure18()
 {
     Play(Ds4);
-    Play(E4); //full note
-    Play(E4); //full note
-    Play(E4); //full note
-    Play(E4); //full note
-    Play(E4); //full note
-    Play(E4); //full note
+    Play(E4); //half note
+    Play(E4); //half note
+    Play(E4); //half note
+    Play(E4); //half note
+    Play(E4); //half note
+    Play(E4); //half note
 }
 
 void PM_measure19()
 {
-    Play(B4); //full note
-    Play(B4); //full note
-    Play(B4); //full note
-    Play(B4); //full note
-    Play(B4); //full note
-    Play(B4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
 }
 
 void PM_measure20()
 {
-    Play(F4); //full note
-    Play(F4); //full note
-    Play(F4); //full note
-    Play(F4); //full note
-    Play(F4); //full note
-    Play(F4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
-    Play(D4); //full note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
 }
 
 void PM_measure21()
@@ -364,6 +366,356 @@ void PM_measure22()
     Play(D3);
 }
 
+void PM_measure23()
+{
+    /*
+     */
+    Play(E4);
+    Play(E4);
+    Play(D4);
+    Play(E4); ///
+    Play(F4);
+    Play(E4);
+    Play(D4);
+    Play(F4); ///
+    Play(E4);
+    Play(E4);
+    Play(Gb4);
+    Play(F4); ///
+    Play(E4);
+    Play(D4);
+    Play(E4);
+    Play(F4); ////
+}
+
+void PM_measure24()
+{
+    Play(E4);
+    Play(E4);
+    Play(D4);
+    Play(E4); ///
+    Play(Ds4);
+    Play(Cs4);
+    Play(C4);
+    Play(D4); //
+    Play(E4);
+    Play(E4);
+    Play(B3);
+    Play(D3);
+}
+
+void PM_measure25()
+{
+    Play(E4);
+    Play(B4);
+    Play(A3);
+    Play(F4);
+    Play(Fs4);
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+}
+
+void PM_measure26()
+{
+    Play(F4);
+    Play(B4);
+    Play(B4);
+    Rest(1);
+    Play(B4);
+    Play(B4);
+    Rest(1);
+    Play(B4);
+}
+
+void PM_measure27()
+{
+    PM_measure25();
+}
+
+void PM_measure28()
+{
+    PM_measure26();
+}
+
+void PM_measure29()
+{
+    Play(Fs4);
+    Play(Cs4);
+    Play(As4);
+    Play(G4);
+    Play(Gs4);
+    Play(G4); //half note
+    Play(G4); //half note
+    Play(G4); //half note
+    Play(G4); //half note
+    Play(G4); //half note
+    Play(G4); //half note
+}
+
+void PM_measure30()
+{
+    Play(G4);
+    Play(Cs4);
+    Play(C4);
+    Rest(1);
+    Play(C4);
+    Play(C4);
+    Rest(1);
+    Play(C4);
+}
+
+void PM_measure31()
+{
+    Rest(1);
+    Play(Cs4);
+    Play(C4);
+    Rest(1);
+    Play(C4);
+    Play(C4);
+    Rest(1);
+    Play(E4);
+    Play(F4);
+}
+
+void PM_measure32()
+{
+    Play(Fs4); //full note
+    Play(Fs4); //full note
+    Play(Fs4); //full note
+    Play(Fs4); //full note
+    Play(Fs4); //full note
+    Play(Fs4); //full note
+    Play(Fs4); //full note
+    Play(Fs4); //full note
+    Play(Fs4); //full note
+    Play(Fs4); //full note
+    Play(Fs4); //full note
+    Play(Fs4); //full note
+}
+
+void PM_measure33()
+{
+    Play(Fs3); //full note
+    Play(Fs3); //full note
+    Play(Fs3); //full note
+    Play(Fs3); //full note
+    Play(Fs3); //full note
+    Play(Fs3); //full note
+    Play(Fs3); //full note
+    Play(Fs3); //full note
+    Play(Fs3); //full note
+    Play(Fs3); //full note
+    Play(Fs3); //full note
+    Play(Fs3); //full note
+}
+
+void PM_measure34()
+{
+    Play(Cs4);
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+}
+
+void PM_measure35()
+{
+    Play(B4);
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+}
+
+void PM_measure36()
+{
+    Play(E4); //full note
+    Play(E4); //full note
+    Play(E4); //full note
+    Play(E4); //full note
+    Play(E4); //full note
+    Play(E4); //full note
+    Play(E4); //full note
+    Play(E4); //full note
+    Play(E4); //full note
+    Play(E4); //full note
+    Play(E4); //full note
+    Play(E4); //full note
+}
+
+void PM_measure37()
+{
+    Play(A4); //full note
+    Play(A4); //full note
+    Play(A4); //full note
+    Play(A4); //full note
+    Play(A4); //full note
+    Play(A4); //full note
+    Play(A4); //full note
+    Play(A4); //full note
+    Play(A4); //full note
+    Play(A4); //full note
+    Play(A4); //full note
+    Play(A4); //full note
+}
+
+void PM_measure38()
+{
+    Play(F4); //full note
+    Play(F4); //full note
+    Play(F4); //full note
+    Play(F4); //full note
+    Play(F4); //full note
+    Play(F4); //full note
+    Play(F4); //full note
+    Play(F4); //full note
+    Play(F4); //full note
+    Play(F4); //full note
+    Play(F4); //full note
+    Play(F4); //full note
+}
+
+void PM_measure39()
+{
+    Play(C5); //half note
+    Play(C5); //half note
+    Play(C5); //half note
+    Play(C5); //half note
+    Play(C5); //half note
+    Play(C5); //half note
+    Play(Bb4); //half note
+    Play(Bb4); //half note
+    Play(Bb4); //half note
+    Play(Bb4); //half note
+    Play(Bb4); //half note
+    Play(Bb4); //half note
+}
+
+void PM_measure40()
+{
+    Play(Cs4);
+    Play(E4); //half note
+    Play(E4); //half note
+    Play(E4); //half note
+    Play(E4); //half note
+    Play(E4); //half note
+    Play(E4); //half note
+}
+
+void PM_measure41()
+{
+    Play(A3);
+    Play(Cs4); //half note
+    Play(Cs4); //half note
+    Play(Cs4); //half note
+    Play(Cs4); //half note
+    Play(Cs4); //half note
+    Play(Cs4); //half note
+}
+
+void PM_measure42()
+{
+    Play(D4);
+    Play(Cs4);
+    Play(B4);
+    Play(C4);
+    Play(D4);
+    Play(C4);
+    Play(Gs4);
+    Play(D4);
+}
+
+void PM_measure43()
+{
+    PM_measure42();
+}
+
+void PM_measure44()
+{
+    PM_measure15();
+}
+
+void PM_measure45()
+{
+    PM_measure16();
+}
+
+void PM_measure46()
+{
+    PM_measure17();
+}
+
+void PM_measure47()
+{
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(C4); //half note
+    Play(C4); //half note
+    Play(C4); //half note
+    Play(C4); //half note
+    Play(C4); //half note
+    Play(C4); //half note
+}
+
+void PM_measure48()
+{
+    PM_measure15();
+}
+
+void PM_measure49()
+{
+    PM_measure16();
+}
+
+void PM_measure50()
+{
+    PM_measure17();
+}
+
+void PM_measure51()
+{
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(D4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+    Play(F4); //half note
+}
+
+void PM_measure52()
+{
+    PM_measure36();
+}
+
+void PM_measure53()
+{
+    Play(As4);
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+    Play(B4); //half note
+}
+
 int main(void)
 {
     init();
@@ -372,7 +724,7 @@ int main(void)
     {
         //https://www.ninsheetmusic.org/browse/arranger/939
         //TestC3Major();
-        setBPM(150);
+        setBPM(140);
 
         //supermariotheme();
 
@@ -385,9 +737,10 @@ int main(void)
         PM_measure5();
         PM_measure6();
 
+        PM_measure7();
         PM_measure8();
-        PM_measure9();
 
+        PM_measure9();
         PM_measure10();
         PM_measure11();
 
@@ -405,51 +758,45 @@ int main(void)
         PM_measure20();
         PM_measure21();
         PM_measure22();
-        /*
-        */
-        Play(E4);
-        Play(E4);
-        Play(D4);
-        Play(E4);///
-        Play(F4);
-        Play(E4);
-        Play(D4);
-        Play(F4);///
-        Play(E4);
-        Play(E4);
-        Play(Gb4);
-        Play(F4);///
-        Play(E4);
-        Play(D4);
-        Play(E4);
-        Play(F4);////
 
-        Play(E4);
-        Play(E4);
-        Play(D4);
-        Play(E4);///
-        Play(Ds4);
-        Play(Cs4);
-        Play(C4);
-        Play(D4);//
-        Play(E4);
-        Play(E4);
-        Play(B3);
-        Play(D3);
+        PM_measure23();
+        PM_measure24();
+        PM_measure25();
 
-        Play(E4);
-        Play(B4);
-        Play(A3);
-        Play(F4);
-        Play(Fs4);
-        Play(F4);//full note
-        Play(F4);//full note
-        Play(F4);//full note
-        Play(F4);//full note
-        Play(F4);//full note
-        Play(F4);//full note
-        Play(F4);//full note
+        PM_measure26();
+        PM_measure27();
+        PM_measure28();
+        PM_measure29();
 
+        PM_measure30();
+        PM_measure31();
+        PM_measure32();
+        PM_measure33();
+
+        PM_measure34();
+        PM_measure35();
+        PM_measure36();
+        PM_measure37();
+        PM_measure38();
+
+
+        PM_measure39();
+        PM_measure40();
+        PM_measure41();
+        PM_measure42();
+        PM_measure43();
+
+        PM_measure44();
+        PM_measure45();
+        PM_measure46();
+        PM_measure47();
+        PM_measure48();
+
+        PM_measure49();
+        PM_measure50();
+        PM_measure51();
+        PM_measure52();
+        PM_measure53();
 
         Rest(10);
 
